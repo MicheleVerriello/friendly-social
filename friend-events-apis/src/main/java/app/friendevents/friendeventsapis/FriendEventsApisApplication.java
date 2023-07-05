75a749a4cfd48a84108e77adc934c97b8dd4cc9e
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-07-05 19:44:48 +0200

    feat(backend): create project

---
+++ friend-events-apis/src/main/java/app/friendevents/friendeventsapis/FriendEventsApisApplication.java
@@ -0,0 +1,13 @@
+package app.friendevents.friendeventsapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendEventsApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendEventsApisApplication.class, args);
+    }
+
+}
---
+++ friend-events-apis/src/test/java/app/friendevents/friendeventsapis/FriendEventsApisApplicationTests.java
@@ -0,0 +1,13 @@
+package app.friendevents.friendeventsapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendEventsApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
