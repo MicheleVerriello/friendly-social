8ab453603f1c416bfc6dceaa9f234bdd19b520fe
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-07-13 23:17:14 +0200

    test(users): add new tests for users

--- friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/UserControllerTest.java
+++ friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/UserControllerTest.java
@@ -1,12 +1,18 @@
 package app.friendevents.friendeventsapis.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import app.friendevents.friendeventsapis.pojos.User;
 import app.friendevents.friendeventsapis.services.UserService;
+import com.google.gson.Gson;
 import jakarta.annotation.security.RunAs;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
@@ -15,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -45,6 +52,47 @@ public class UserControllerTest {
         Mockito.verify(userService, Mockito.times(1)).getUserById(1L);
     }
 
+//    @Test
+//    public void getUserById_NotFound() throws Exception {
+//
+//        Mockito.when(userService.getUserById(1L)).thenReturn(Optional.empty());
+//
+//        mockMvc.perform(get("/users/1")).andExpect(status().isNotFound());
+//
+//        Mockito.verify(userService, Mockito.times(1)).getUserById(1L);
+//    }
+
     @Test
-    public void createUser() {}
+    public void createUser() throws Exception {
+
+
+
+        
+        Gson gson = new Gson();
+
+        User user = new User(
+                "email1@mail.com",
+                "username1",
+                "pass1",
+                null,
+                "name1",
+                "surname1"
+        );
+
+        String jsonUser = gson.toJson(user);
+        System.out.println(jsonUser);
+
+        User userResponse = user;
+        userResponse.setId(1L);
+        userResponse.setCreationDate(new Timestamp(Instant.now().toEpochMilli()));
+
+        Mockito.when(userService.createUser(user)).thenReturn(userResponse);
+
+        mockMvc.perform(post("/users/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonUser))
+                .andExpect(status().isOk());
+
+        Mockito.verify(userService, Mockito.times(1)).createUser(Mockito.any());
+    }
 }
--- friend-events-apis/src/test/java/app/friendevents/friendeventsapis/repositories/tests/UserRepositoryTest.java
+++ friend-events-apis/src/test/java/app/friendevents/friendeventsapis/repositories/tests/UserRepositoryTest.java
@@ -2,27 +2,26 @@ package app.friendevents.friendeventsapis.repositories.tests;
 
 import app.friendevents.friendeventsapis.pojos.User;
 import app.friendevents.friendeventsapis.repositories.IUserRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-
 @SpringBootTest
 public class UserRepositoryTest {
-
     @Autowired
     private IUserRepository userRepository;
 
     @Test
     public void findById() {
         User user = new User(
-                "email1@mail.com",
-                "username1",
-                "pass1", null,
-                "name1",
-                "surname1"
+            "email1@mail.com",
+            "username1",
+            "pass1",
+            null,
+            "name1",
+            "surname1"
         );
 
         User userResult = userRepository.save(user);
