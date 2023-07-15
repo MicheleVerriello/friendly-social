6a2aa6b512afcec828ebba79ee41282e434c52a4
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-07-15 16:11:11 +0200

    test: add generic controller test

---
+++ friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/GenericControllerTest.java
@@ -0,0 +1,25 @@
+package app.friendevents.friendeventsapis.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GenericControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void ping() throws Exception {
+        mockMvc.perform(get("/generic/ping"))
+                .andExpect(status().isOk());
+    }
+}
