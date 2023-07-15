85af0bfd72cc029d7d66ee6a7444eec66c5b7346
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-07-15 16:11:43 +0200

    refactor: formatting

--- friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/GenericControllerTest.java
+++ friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/GenericControllerTest.java
@@ -1,5 +1,8 @@
 package app.friendevents.friendeventsapis.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,19 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 public class GenericControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void ping() throws Exception {
-        mockMvc.perform(get("/generic/ping"))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/generic/ping")).andExpect(status().isOk());
     }
 }
