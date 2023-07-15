8dfe127565553a70bae09c6325a194fba0f5c7ee
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-07-15 16:18:02 +0200

    refactor: add comments

--- friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/GenericControllerTest.java
+++ friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/GenericControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,6 +15,11 @@ public class GenericControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    /**
+     * Test for ping endpoint
+     *
+     * @throws Exception handling exception for mockMvc.perform()
+     */
     @Test
     public void ping() throws Exception {
         mockMvc.perform(get("/generic/ping")).andExpect(status().isOk());
