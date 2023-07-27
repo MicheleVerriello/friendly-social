cf4bd73e823af739c4373346c27b8a914a302ddd
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-07-27 17:09:23 +0200

    feat(logs): add test log

--- friend-events-apis/src/main/java/app/friendevents/friendeventsapis/controllers/GenericController.java
+++ friend-events-apis/src/main/java/app/friendevents/friendeventsapis/controllers/GenericController.java
@@ -1,5 +1,7 @@
 package app.friendevents.friendeventsapis.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/generic")
 public class GenericController {
 
+    private static final Logger logger = LoggerFactory.getLogger(GenericController.class);
+
+
     /**
      * Simple endpoint to ping the app and see if it's running.
      * @return The app status
      */
     @GetMapping("/ping")
     public String ping() {
+        logger.info("Reached ping endpoint");
         return "App Live On AWS EC2, Hello!";
     }
 }
