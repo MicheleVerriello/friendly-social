28705392030df4a1444a633f4738b7d8792eb254
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2023-07-28 19:10:31 +0200

    feat(logs): add cloudwatch dependency

--- friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/GenericControllerTest.java
+++ friend-events-apis/src/test/java/app/friendevents/friendeventsapis/controllers/GenericControllerTest.java
@@ -3,17 +3,35 @@ package app.friendevents.friendeventsapis.controllers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class GenericControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @InjectMocks
+    private GenericController genericController;
+
+    @Mock
+    private Logger logger;
+
+    @BeforeTestClass
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(genericController).build();
+    }
 
     /**
      * Test for ping endpoint
